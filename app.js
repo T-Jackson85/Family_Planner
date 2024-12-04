d5b783579998227cd8d770761b6c43c56bfa5bb6
Tracee <jackson.tracee@yahoo.com>
2024-12-04 09:28:22 -0500

    updated capstone

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const eventRoutes = require('./routes/eventRoutes'); 
+const expenseRoutes = require('./routes/expenseRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const groupRoutes = require('./routes/groupRoutes');
 
-const prisma = new PrismaClient();
 const app = express();
 
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use('/api', userRoutes);
+app.use('/api', eventRoutes);
+app.use('/api', expenseRoutes);
+app.use('/api', taskRoutes);
+app.use('/api', groupRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 
